3e3fe3d6c9e00ed200e9be519c0f39268bab20fd
Sardar balnour <balnour.sardar@gmail.com>
2026-05-28 16:58:40 +0330

    clean code

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 
 import ContactsList from "./ContactsList";
+import inputs from "../constants/inputs";
+
+
 
 function Contacts() {
   const [contacts, setContacts] = useState([]);
@@ -42,34 +45,17 @@ function Contacts() {
   return (
     <div>
       <div>
-        <input
-          type="text"
-          name="name"
-          value={contact.name}
-          placeholder="Name"
-          onChange={changeHandler}
-        />
-        <input
-          type="text"
-          name="lastName"
-          value={contact.lastName}
-          placeholder="Last name"
-          onChange={changeHandler}
-        />
-        <input
-          type="email"
-          name="email"
-          value={contact.email}
-          placeholder="Email"
-          onChange={changeHandler}
-        />
-        <input
-          type="number"
-          name="phoneNumber"
-          value={contact.phoneNumber}
-          placeholder="Phone number"
-          onChange={changeHandler}
-        />
+        {inputs.map((input,index) => (
+          <input
+            name={input.name}
+            type={input.type}
+            placeholder={input.placeholder}
+            key={index}
+            value={contact[input.name]}
+            onChange={changeHandler}
+          />
+        ))}
+
         <button onClick={addHandler}>Add contact</button>
       </div>
       <div>{alert && <p>{alert}</p>}</div>
---
+++ src/constants/inputs.js
@@ -0,0 +1,8 @@
+const inputs = [
+  { type: "text", name: "name", placeholder: "Name :" },
+  { type: "text", name: "lastName", placeholder: "Last name :" },
+  { type: "email", name: "email", placeholder: "Email :" },
+  { type: "number", name: "phoneNumber", placeholder: "Phone number :" },
+];
+
+export default inputs;
